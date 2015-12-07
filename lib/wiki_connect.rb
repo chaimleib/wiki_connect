5ba2a1e9a0e010e39fa27d063f577ccbf75792c0
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-12-07 01:59:17 -0800

    began wiki_connect.rb

---
+++ lib/wiki_connect.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'uri'
+
+class WikiConnect
+  def initialize(config)
+    @config = config.dup
+    @config['host'] = URI.parse(@config['host'])
+  end
+
+  def revisions(params)
+    params = params.dup.merge!(@config.params)
+  end
+
+  def path_from_params(params)
+    @config['path'] + '?' + params.each_with_object([]) do |(k,v), rv|
+      uri_key = URI.encode k
+      if v.is_a? Array
+        uri_val = URI.encode v
+      else
+        uri_val = v.map{|el| URI.encode(el)}.join('%7C')
+      end
+    end.join('&')
+  end
+
+  def uri_from_params(params)
+    @config['host'].join path_from_params(params)
+  end
+end
+
