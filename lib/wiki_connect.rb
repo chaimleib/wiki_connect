853bbb59e9ca6117b38e2bce20e5891d41e1825b
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-12-07 03:05:45 -0800

    fix wiki_connect.rb to return proper URI

--- lib/wiki_connect.rb
+++ lib/wiki_connect.rb
@@ -4,30 +4,46 @@ require 'uri'
 class WikiConnect
   def initialize(config)
     @config = config.dup
-    @config['host'] = URI.parse(@config['host'])
   end
 
   def source(params)
-    params = params.dup.merge!(@config['source'].params)
+    params = params.dup.merge!(@config['source']['params'])
+    uri = uri_from_params(params, 'source')
   end
 
   def revisions(params)
-    params = params.dup.merge!(@config['revisions'].params)
+    params = params.dup.merge!(@config['revisions']['params'])
+    uri = uri_from_params(params, 'revisions')
   end
 
-  def path_from_params(params)
-    @config['path'] + '?' + params.each_with_object([]) do |(k,v), rv|
-      uri_key = URI.encode k
+  def defaults(key, cmd)
+    if @config.has_key?(cmd) && @config[cmd].has_key?(key)
+      return @config[cmd][key]
+    elsif @config.has_key?('defaults') && @config['defaults'].has_key?(key)
+      return @config['defaults'][key]
+    else
+      raise "ERROR: WikiConnect defaults: no such key `#{key}` in group `#{cmd}`"
+    end
+  end
+
+  def path_from_params(params, group)
+    rv = []
+    rv << defaults('path', group)
+    rv << '?'
+    rv << params.map do |(k,v)|
+      uri_key = URI.encode k.to_s
       if v.is_a? Array
-        uri_val = URI.encode v
+        uri_val = v.map{|el| URI.encode(el.to_s)}.join('%7C')
       else
-        uri_val = v.map{|el| URI.encode(el)}.join('%7C')
+        uri_val = URI.encode v.to_s
       end
+      uri_key + '=' + uri_val
     end.join('&')
+    rv.join
   end
 
-  def uri_from_params(params)
-    @config['host'].join path_from_params(params)
+  def uri_from_params(params, group)
+    defaults('host', group) + path_from_params(params, group)
   end
 end
 
