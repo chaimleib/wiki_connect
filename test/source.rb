9daa3c06aba7d96b49bcb39b571ddfc3d536d84b
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-12-07 03:06:02 -0800

    test source method

---
+++ test/source.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+require 'awesome_print'
+require_relative '../lib/wiki_connect'
+
+this_dir = File.expand_path File.dirname __FILE__
+opts = YAML.load_file File.join(this_dir, '../config/defaults.yml')
+
+wc = WikiConnect.new opts
+ap wc.source title: 'Trie'
+
